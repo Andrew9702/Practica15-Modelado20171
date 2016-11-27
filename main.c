786112aedc8b2ab2df75476ec48a43cc9ee597e2
AndresBarbosa <andresbob@ciencias.unam.mx>
2016-11-26 19:58:22 -0600

    Funciona con un archivo y con una linea por el momento

---
+++ main.c
@@ -0,0 +1,134 @@
+#include "main.h"
+// No hagas #include aquí, hazlos en main.h
+
+int main(int argc, char const *argv[])
+{
+	//Creamos una lista en donde guardaremos las palabras del archivo
+	Lista palabras = crea_lista();
+
+    // Verifica si hay al menos de 3 parametros
+    // sino entonces se imprimen las instrucciones
+    // y se para la ejecución del programa
+    if (argc < 2){
+        imprime_instrucciones();
+        return 0;
+
+    } else {
+    	int indice = 1;
+    	FILE *archivo;
+    	//Abrimos todos los archivos que se le pasan como parametros
+    	for(indice;argv[indice]!=NULL;indice++){
+    		archivo = fopen(argv[indice], "a+");
+    		printf("Abriendo el archivo %s\n", argv[indice]);
+    		rellena(palabras, archivo);
+
+    	}
+
+    }
+    return 0; 
+}
+
+/* ------ Aquí va la implementación de tus funciones ------ */
+void imprime_instrucciones(){
+	//La funcion puts solo imprime el output en la consola
+ 	puts("\n   Error: El formato de archivo es incorrecto\n   Uso: Nombre del ejecutable <archivo> [<archivo> [<archivo> ...]] <archivo>");
+    return;
+}
+
+/*Inserta un elemento en la lista y se regresa el nuevo tamaño de la lista
+  Los elementos se agregan al inicio de la lista, como una pila
+*/
+void inserta_elemento(Lista lista, Palabra *palabra){
+	int indice =0;
+	Elemento* doit = *lista;
+	int found = 0;
+	while(doit!=NULL){
+		if(strcmp(doit->palabra->word, palabra->word) ==0 ){
+			doit->palabra->apariciones +=1;
+			found = 1;
+			break;
+		} 
+		doit= doit->siguiente;
+	}
+
+	if(!found){
+		//Creamos un nuevo elemento
+		Elemento *recien = malloc(sizeof(Elemento));
+		//Le damos direcciones a este nuevo elemento
+		recien->siguiente = *lista;
+		//Igualamos el valor del nuevo elemento a la nueva palabra
+		recien->palabra = palabra;
+		//Apuntamos la cabeza al nuevo elemento
+		*lista = recien;
+	}		
+}
+
+/*Imprime los elementos de la lista como cadenas*/
+void imprime_lista(Lista lista){
+	int indice = 0;
+	Elemento *aux = *lista;
+	indice =0;
+	for(indice;indice<longitud(lista);indice++){
+		Palabra *valorCabeza = aux->palabra;
+		printf("la palabra en el indice %d es:  %s, aparece %d veces \n", indice, valorCabeza->word, valorCabeza->apariciones);
+		aux = aux->siguiente;
+	} 
+}
+
+/*Crea una lista vacía*/
+Lista crea_lista(){
+	//Se aparta el espacio en el heap para la lista
+	Lista lista = calloc(1 ,sizeof(Lista));
+	return lista;
+}
+
+/*Regresa el número de elementos en la lista*/
+size_t longitud(Lista lista){
+	if(*lista == NULL){
+		return 0;
+	} else{
+		size_t size = 0;
+		Elemento* derecho = *lista;
+		while(derecho != NULL){
+			derecho = derecho->siguiente;
+			size+=1;
+		}
+        return size;
+	}
+}
+
+void rellena(Lista palabras, FILE *archivo){
+    // Se lee del archivo que este en indice
+    char buffer[100];
+    while(fgets(buffer, 100, archivo)) {
+    	char *token_actual = strtok(buffer, " ,.-");
+		while(token_actual != NULL) {
+			Palabra *segmento = malloc(sizeof(Palabra));
+    		segmento -> apariciones = 1;
+    		segmento -> word = token_actual;
+			inserta_elemento(palabras, segmento);
+       		token_actual = strtok (NULL, " ,.-");
+    	}
+    	//imprime_lista(palabras);
+    	printf("..................TU LISTA.....................\n");
+    	imprime_lista(palabras);
+    }
+}
+/**
+void obtiene(Lista palabras, FILE *archivo){
+    // Se lee del archivo que este en indice
+    char buffer[100];
+    Palabra *segmento;
+    while(fgets(buffer, 100, archivo)) {
+    	char *token_actual = strtok(buffer, " ,.-");
+			while(token_actual != NULL) {
+        		printf ("PALABRA: %s\n",token_actual);
+				//printf("valor para segmento en word! %s\n", segmento->word);
+				inserta_elemento(palabras, segmento);
+        		token_actual = strtok (NULL, " ,.-");
+    		}
+    		//imprime_lista(palabras);
+    		printf("..................Empezar de NUEVO.....................\n");
+    }
+}
+**/
